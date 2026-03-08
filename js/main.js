33b7aa8a83324a297a0678cc39acd655b15971f0
Your Name <you@example.com>
2026-03-08 12:53:09 +0300

    end verstka catalog menu and page tovar

--- js/main.js
+++ js/main.js
@@ -1,22 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // menu open/close
+  const catalogMenuItems = document.querySelectorAll('.catalog-menu-list__item')
   const catalogMenuButtons = document.querySelectorAll('.catalog-menu-list__img')
   const catalogInMenuLists = document.querySelectorAll('.catalog-in-menu-list')
 
-  catalogMenuButtons.forEach((item, i) => {
+  catalogMenuItems.forEach((item, i) => {
     item.addEventListener('click', () => {
       // clear class ">"
-      catalogMenuButtons.forEach(item => {
-        item.classList.contains('catalog-menu-list__img--active') ? item.classList.remove('catalog-menu-list__img--active') : ''
+      catalogMenuButtons.forEach(itemButton => {
+        itemButton.classList.contains('catalog-menu-list__img--active') ? itemButton.classList.remove('catalog-menu-list__img--active') : ''
       })
 
       // clear class "in ul"
-      catalogInMenuLists.forEach(item => {
-        item.classList.contains('catalog-in-menu-list--active') ? item.classList.remove('catalog-in-menu-list--active') : ''
+      catalogInMenuLists.forEach(itemList => {
+        itemList.classList.contains('catalog-in-menu-list--active') ? itemList.classList.remove('catalog-in-menu-list--active') : ''
       })
 
-      item.classList.contains('catalog-menu-list__img--active') ? '' : item.classList.add('catalog-menu-list__img--active')
+      catalogMenuButtons[i].classList.contains('catalog-menu-list__img--active') ? '' : catalogMenuButtons[i].classList.add('catalog-menu-list__img--active')
 
       catalogInMenuLists[i].classList.contains('catalog-in-menu-list--active') ? '' : catalogInMenuLists[i].classList.add('catalog-in-menu-list--active')
     })
   })
+
+  // catalogMenuButtons.forEach((item, i) => {
+  //   item.addEventListener('click', () => {
+  //     // clear class ">"
+  //     catalogMenuButtons.forEach(item => {
+  //       item.classList.contains('catalog-menu-list__img--active') ? item.classList.remove('catalog-menu-list__img--active') : ''
+  //     })
+
+  //     // clear class "in ul"
+  //     catalogInMenuLists.forEach(item => {
+  //       item.classList.contains('catalog-in-menu-list--active') ? item.classList.remove('catalog-in-menu-list--active') : ''
+  //     })
+
+  //     item.classList.contains('catalog-menu-list__img--active') ? '' : item.classList.add('catalog-menu-list__img--active')
+
+  //     catalogInMenuLists[i].classList.contains('catalog-in-menu-list--active') ? '' : catalogInMenuLists[i].classList.add('catalog-in-menu-list--active')
+  //   })
+  // })
 });
