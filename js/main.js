acf6aa74289ab9505b32c3816a6f492fee7a034e
Your Name <you@example.com>
2026-04-04 16:51:14 +0300

    СОХРАНЯЙ ТЕПЕРЬ ВСЁ

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const catalogMenuItems = document.querySelectorAll('.catalog-menu-list__item')
   const catalogMenuButtons = document.querySelectorAll('.catalog-menu-list__img')
   const catalogInMenuLists = document.querySelectorAll('.catalog-in-menu-list')
+  const catalog2InMenuLists = document.querySelectorAll('.catalog-2in-menu-list')
+  const catalog2InBox = document.querySelectorAll('.catalog-in-menu-list-item__box')
+  const catalog2img = document.querySelectorAll('.catalog-in-menu-list__img')
 
   catalogMenuItems.forEach((item, i) => {
     item.addEventListener('click', () => {
@@ -22,23 +25,23 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   })
 
-  // catalogMenuButtons.forEach((item, i) => {
-  //   item.addEventListener('click', () => {
-  //     // clear class ">"
-  //     catalogMenuButtons.forEach(item => {
-  //       item.classList.contains('catalog-menu-list__img--active') ? item.classList.remove('catalog-menu-list__img--active') : ''
-  //     })
+  catalog2InBox.forEach((item, i) => {
+    item.addEventListener('click', () => {
+      // clear class ">"
+      catalog2img.forEach(itemButton => {
+        itemButton.classList.contains('catalog-menu-list__img--active') ? itemButton.classList.remove('catalog-menu-list__img--active') : ''
+      })
 
-  //     // clear class "in ul"
-  //     catalogInMenuLists.forEach(item => {
-  //       item.classList.contains('catalog-in-menu-list--active') ? item.classList.remove('catalog-in-menu-list--active') : ''
-  //     })
+      // clear class "in ul"
+      catalog2InMenuLists.forEach(itemList => {
+        itemList.classList.contains('catalog-in-menu-list--active') ? itemList.classList.remove('catalog-in-menu-list--active') : ''
+      })
 
-  //     item.classList.contains('catalog-menu-list__img--active') ? '' : item.classList.add('catalog-menu-list__img--active')
+      catalog2img[i].classList.contains('catalog-menu-list__img--active') ? '' : catalog2img[i].classList.add('catalog-menu-list__img--active')
 
-  //     catalogInMenuLists[i].classList.contains('catalog-in-menu-list--active') ? '' : catalogInMenuLists[i].classList.add('catalog-in-menu-list--active')
-  //   })
-  // })
+      catalog2InMenuLists[i].classList.contains('catalog-in-menu-list--active') ? '' : catalog2InMenuLists[i].classList.add('catalog-in-menu-list--active')
+    })
+  })
 
   // burger
   const burgerMenu = document.querySelector('.burger-menu')
@@ -87,50 +90,5 @@ document.addEventListener("DOMContentLoaded", () => {
     modalCall.classList.remove('call-modal--active') :
     "" 
   })
-
-  // Получаем элементы
-// const form = document.querySelector('.call-modal__form')
-// const modal = document.querySelector('.form-modal');
-// const modalMessage = document.querySelector('.modal-message');
-
-// // Обработчик отправки формы
-// form.addEventListener('submit', function(event) {
-//   event.preventDefault(); // Отменяем стандартную отправку
-
-//   return;
-//   // Собираем данные формы
-//   const formData = new FormData(form);
-
-//   // Отправляем на сервер через fetch
-//   fetch('php/send.php', {
-//     method: 'POST',
-//     body: formData
-//   })
-//   .then(response => response.text()) // Получаем ответ как текст
-//   .then(data => {
-//     // Обрабатываем ответ от сервера
-//     if (data.includes('Письмо отправлено!')) {
-//       showModal('Сообщение успешно отправлено!', 'success');
-//     } else {
-//       showModal('Ошибка отправки. Попробуйте ещё раз.', 'error');
-//     }
-//   })
-//   .catch(error => {
-//     showModal('Произошла ошибка сети.', 'error');
-//   });
-// });
-
-// // Функция для показа модального окна
-// function showModal(message, type) {
-//   modalMessage.textContent = message;
-//   modal.style.display = 'flex';
-
-//   // Можно добавить класс для стилизации (например, красный/зелёный текст)
-//   if (type === 'success') {
-//     modalMessage.style.color = 'green';
-//   } else {
-//     modalMessage.style.color = 'red';
-//   }
-// }
 
 });
